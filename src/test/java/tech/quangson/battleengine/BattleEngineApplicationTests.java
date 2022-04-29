b0f7998ca496419a651051e7ec7b84668642e110
Mauvecrow <64569782+mauvecrow@users.noreply.github.com>
2022-04-29 15:53:49 -0600

    initial test from Spring Boot starter

---
+++ src/test/java/tech/quangson/battleengine/BattleEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.quangson.battleengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattleEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
