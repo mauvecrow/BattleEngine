c9f5bff1052fefc3e8b0dda66acb5b3502167049
Mauvecrow <64569782+mauvecrow@users.noreply.github.com>
2022-04-29 14:57:07 -0600

    Initial commit

---
+++ src/main/java/tech/quangson/battleengine/BattleEngineApplication.java
@@ -0,0 +1,13 @@
+package tech.quangson.battleengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattleEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BattleEngineApplication.class, args);
+    }
+
+}
